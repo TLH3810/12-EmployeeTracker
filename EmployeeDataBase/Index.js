10ea8547c0e041a45697f6b4284f9425e7cecb8c
Yorkie Fan <terahowell@gmail.com>
2021-08-04 19:03:33 -0400

    able to return both the first and second query

---
+++ EmployeeDataBase/Index.js
@@ -0,0 +1,88 @@
+const inquirer = require("inquirer");
+
+
+class empDbQ {
+    constructor(connection) {
+        this.connection = connection;
+    }
+
+    //View departments, 
+    selectAllDepartments() {
+        this.connection.query("SELECT * FROM departments", (err, results) => {
+            if (err) {
+                console.log(error);
+            } else {
+                console.log(results)
+            }
+        });
+    }
+
+    //View roles, 
+    selectAllEmpRoles() {
+        this.connection.query('SELECT * FROM employeeRole', (err, results) => {
+            if (err) {
+                console.log(error);
+            } else {
+                console.log(results)
+            }
+        });
+    }
+
+    //View employees
+    selectAllEmployees(){
+        this.connection.query('SELECT * FROM employee', (err, results) => {
+            if (err) {
+                console.log(error);
+            } else {
+                console.log(results)
+            }  
+        });
+    }
+
+
+    //Add departments, roles, employees
+    insertAddEmployee(){
+
+            inquirer.prompt(
+                {
+                message: "Please enter the Employee First Name ",
+                type: "input",
+                name:"firstName"
+            },
+            {
+                message: "Please enter the Employee Last Name ",
+                type: "input",
+                name:"lastName"
+            },
+            {
+                message: "Please enter the Employee Role ID ",
+                type: "input",
+                name:"roleId"
+            },
+            {
+                message: "Please enter the Employee Manager ID ",
+                type: "input",
+                name:"managerId"
+            },
+            );
+            .then(answer){
+                var fname = answer.firstName;
+                var lname = answer.lastName;
+                var roleId = answer.roleId;
+                var mgid = answer.managerId;
+                this.connection.query('INSERT INTO employee SET ?',[{first_name:fname,last_name:lname,role_Id:roleId,manager_Id:mgid}]);    
+            }
+             
+        });
+
+    
+    insertAddDepartment(){}
+    insertAddRole(){}
+
+//Update employee roles
+    updateEmployeeRoll(){}
+
+
+};
+
+module.exports = empDbQ;
--- Index.js
+++ Index.js
@@ -1,6 +1,6 @@
 const inquirer = require("inquirer");
 const { prompt } = require("inquirer");
-const db = require("./EmployeeDataBase/employeeQueryIndex");
+const db = require("./EmployeeDataBase/Index");
 //const { inherits } = require("node:util");
 const connection = require("./EmployeeDataBase/employeeConnectDb");
 
@@ -9,24 +9,28 @@ function loadQuestionPrompt() {
     inquirer
         .prompt({
             name: 'action',
-            type: 'rawlist',
+            type: 'list',
             message: 'What HR query would you like to run',
             choices: [
                 'Find All Employees Roles',
-
+                'Find All Employees'
             ],
         })
-
         .then((answer) => {
+            const newDB = new db(connection);
             switch (answer.action) {
                 case 'Find All Employees Roles':
-                    const newDB = new db(connection);
-                    newDB.selectAllEmpRoles();
                    
+                    newDB.selectAllEmpRoles();
                     break;
+                case 'Find All Employees':
+                   newDB.selectAllEmployees();
+                   break;
             }
+           // return answer;
         });
-
+       
+                
 };
 
 loadQuestionPrompt();
